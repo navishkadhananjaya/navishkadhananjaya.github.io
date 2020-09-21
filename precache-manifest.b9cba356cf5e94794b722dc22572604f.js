ae7e38bd057a5798a3b25034632db854d5eec284
NavishkaDhananjaya <pereranavishka@gmail.com>
2020-09-21 17:00:54 +0530

    Official Page v1.0

---
+++ precache-manifest.b9cba356cf5e94794b722dc22572604f.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781d1f9ee74357f0184609612073b9fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6744c8425e0ac5cf2be",
+    "url": "/static/css/2.9497d81f.chunk.css"
+  },
+  {
+    "revision": "eea9363c66a438bc89e5",
+    "url": "/static/css/main.2db2e869.chunk.css"
+  },
+  {
+    "revision": "d6744c8425e0ac5cf2be",
+    "url": "/static/js/2.10454fdc.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.10454fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eea9363c66a438bc89e5",
+    "url": "/static/js/main.dfed8e06.chunk.js"
+  },
+  {
+    "revision": "d3124b27bbb5b4089688",
+    "url": "/static/js/runtime-main.7974d308.js"
+  },
+  {
+    "revision": "9d138cf1496cdbbb53bfcd01f7367d18",
+    "url": "/static/media/Mine.9d138cf1.jpg"
+  },
+  {
+    "revision": "baf3098516cf3f3e67e6ca3161a840ed",
+    "url": "/static/media/MyProfile.baf30985.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "491a992b497bebfef0441663272efd07",
+    "url": "/static/media/city.491a992b.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "a694c3a6adf09a268243f3fd2d966625",
+    "url": "/static/media/grocery.a694c3a6.jpg"
+  },
+  {
+    "revision": "86622c2f7b7cf442beb92f3b400e47a9",
+    "url": "/static/media/lan.86622c2f.jpg"
+  },
+  {
+    "revision": "f9e17537478609e12c32881528f92153",
+    "url": "/static/media/library.f9e17537.jpg"
+  },
+  {
+    "revision": "ae3c245be7f345995e723de981c2092e",
+    "url": "/static/media/music.ae3c245b.jpg"
+  },
+  {
+    "revision": "cfcbde3a71aa8ab7700b1909304cbf41",
+    "url": "/static/media/navishka_dhananjaya_cv.cfcbde3a.pdf"
+  },
+  {
+    "revision": "30a9e4234325e913340e3d7dbdd88cf1",
+    "url": "/static/media/order.30a9e423.jpg"
+  },
+  {
+    "revision": "f0834af37fea331ba76df65d43a8efa6",
+    "url": "/static/media/pharmacy.f0834af3.jpg"
+  },
+  {
+    "revision": "e0961d54ee65b715b0129d8a2fb112be",
+    "url": "/static/media/phoenix.e0961d54.jpg"
+  }
+]);
